e03bfdef01ee2e5dc12fc6d6442fd8fb3eb2e0b8
Eric Kunzel <kunzel.eric@gmail.com>
2024-07-23 20:09:54 -0300

    feat: created new route for search movie page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from '../components/home/home.component';
+import { HomeComponent as HomePage } from '../pages/home/home.component';
+import { SearchMoviesPageComponent as SearchPage } from '../pages/search-movies-page/search-movies-page.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
-    component: HomeComponent,
+    component: HomePage,
+  },
+  {
+    path: 'search-movies',
+    component: SearchPage,
   }
 ];
